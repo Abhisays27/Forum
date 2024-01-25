f8692221ad52be145c237b6e3c30b88295b525e0
abhishek <abhishek.nt94@gmail.com>
2024-01-25 07:14:19 -0800

    fixed

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function toggleReplySection() {
     icon.toggleClass('fa-chevron-down fa-chevron-up');
 }
 
+window.onscroll = function() {
+    var header = document.getElementById("header");
+    var h1 = header.querySelector("h1");
+
+    if (window.scrollY > header.offsetTop) {
+        h1.classList.add("h1-fixed");
+    } else {
+        h1.classList.remove("h1-fixed");
+    }
+};
